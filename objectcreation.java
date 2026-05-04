c4d15b6adea5f3da56fba2d5d5c1253d9744f4ef
Prakruthisd18 <prakruthisd18@gmail.com>
2026-05-04 23:04:23 +0530

    Add files via upload

---
+++ objectcreation.java
@@ -0,0 +1,14 @@
+package day7;
+
+public class objectcreation {
+	int age=20;
+	String name="Prakruthi";
+
+	public static void main(String[] args) {
+		objectcreation s =new objectcreation();
+		System.out.println(s.age);
+		System.out.println(s.name);
+
+	}
+
+}
